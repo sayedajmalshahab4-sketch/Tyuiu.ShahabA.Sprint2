3ff516620c4349ba997e7d9e624c94ddd45bfb2b
Ajmal <Ajmal@DESKTOP-01V489G>
2025-10-28 10:32:46 -0700

    Add project files.

---
+++ Tyuiu.ShahabA.Sprint2.Task0.V6.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShahabAJ.Sprint2.Task0.V6.Lib;
+
+namespace Tyuiu.ShahabAJ.Sprint2.Task0.V6.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetCompareOperations()
+        {
+            DataService ds = new DataService();
+            int x = 107;
+            int y = 754;
+            bool[] wait = { true, true, true, false, true, false };
+            bool[] result = ds.GetCompareOperations(x, y);
+            CollectionAssert.AreEqual(wait, result);
+        }
+    }
+}
