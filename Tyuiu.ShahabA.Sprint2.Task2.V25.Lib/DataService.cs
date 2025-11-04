cf466bb488e13d0ab2bb22062036b29a2b897a39
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-04 00:09:27 -0800

    Task2

---
+++ Tyuiu.ShahabA.Sprint2.Task2.V25.Lib/DataService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShahabA.Sprint2.Task2.V25.Lib
+{
+    public class DataService :ISprint2Task2V25
+    {
+        public bool CheckDotInShadedArea(int x, int y)
+        {
+            bool result = false;
+
+            // Основная область - большой прямоугольник
+            if ((x >= 3 && x <= 5) && (y >= 3 && y <= 7))
+            {
+                result = true;
+            }
+            // Правая область
+            else if ((x >= 6 && x <= 7) && (y >= 4 && y <= 5))
+            {
+                result = true;
+            }
+            // Нижняя область
+            else if ((x >= 8 && x <= 9) && (y >= 3 && y <= 4))
+            {
+                result = true;
+            }
+            // Левая область
+            else if ((x >= 10 && x <= 12) && (y >= 3 && y <= 7))
+            {
+                result = true;
+            }
+            // Верхняя область
+            else if ((x >= 9 && x <= 11) && (y >= 5 && y <= 6))
+            {
+                result = true;
+            }
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.ShahabA.Sprint2.Task2.V25/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Tyuiu.ShahabA.Sprint2.Task2.V25.Lib;
+
+namespace Tyuiu.ShahabA.Sprint2.Task2.V25
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.WriteLine("*************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("*************************");
+
+            Console.Write("Введите координату X: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите координату Y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("*************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                             *");
+            Console.WriteLine("*************************");
+
+            bool inArea = ds.CheckDotInShadedArea(x, y);
+
+            if (inArea)
+            {
+                Console.WriteLine($"Точка ({x}, {y}) находится в заштрихованной области");
+            }
+            else
+            {
+                Console.WriteLine($"Точка ({x}, {y}) НЕ находится в заштрихованной области");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
