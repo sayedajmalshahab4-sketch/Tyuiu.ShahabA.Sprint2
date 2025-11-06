de6b3a1c7a35058a3dd84346296f865bbe0266c6
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-06 07:42:49 -0800

    Task7

---
+++ Tyuiu.ShahabA.Sprint2.Task7.V1.Lib/DataService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using tyuiu.cources.programming.interfaces.Sprint2;
+
+namespace Tyuiu.ShahabA.Sprint2.Task7.V1.Lib
+{
+    public class DataService :ISprint2Task7V1
+    {
+        public bool CheckDotInShadedArea(double x, double y)
+        {
+            // Проверка нахождения точки между линиями y = x и y = -x
+            // в области где |y| <= |x|
+            bool result = (y <= Math.Abs(x)) && (y >= -Math.Abs(x));
+
+            return result;
+        }
+    }
+}
---
+++ Tyuiu.ShahabA.Sprint2.Task7.V1.Test/DataServiceTest.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tyuiu.ShahabA.Sprint2.Task7.V1.Lib;
+
+namespace Tyuiu.ShahabA.Sprint2.Task7.V1.Test
+{
+    [TestClass]
+    public class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCheckDotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 2;
+            double y = 1;
+            bool wait = true;
+            bool result = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, result);
+        }
+
+        [TestMethod]
+        public void ValidCheckDotNotInShadedArea()
+        {
+            DataService ds = new DataService();
+            double x = 1;
+            double y = 2;
+            bool wait = false;
+            bool result = ds.CheckDotInShadedArea(x, y);
+            Assert.AreEqual(wait, result);
+        }
+    }
+}
