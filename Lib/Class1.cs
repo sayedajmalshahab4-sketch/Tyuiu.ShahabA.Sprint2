1494f6dc88c76d537163032aa0aadc6a1599a9fe
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 17:16:02 +0500

    Task4

---
+++ Lib/Class1.cs
@@ -0,0 +1,11 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint2;
+namespace Lib
+{
+    public class Class1 : ISprint2Task0V6
+    {
+        public bool[] GetCompareOperations(int x, int y)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
