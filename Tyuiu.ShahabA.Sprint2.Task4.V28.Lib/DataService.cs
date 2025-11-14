25d369d449d2b4831f37597386ad666d3d6279a2
Ajmal <Ajmal@DESKTOP-01V489G>
2025-11-14 17:25:30 +0500

    Task4

--- Tyuiu.ShahabA.Sprint2.Task4.V28.Lib/DataService.cs
+++ Tyuiu.ShahabA.Sprint2.Task4.V28.Lib/DataService.cs
@@ -7,8 +7,8 @@ namespace Tyuiu.ShahabA.Sprint2.Task4.V28.Lib
         public double Calculate(double x, double y)
         {
             double z = (x < y * 2 - 10)
-                ? Math.Pow(1 + (x * 2) / (x * 2), x)  // z = (1 + x²/x²)^x
-                : (2 * Math.Pow(x, 2) - Math.Pow(Math.Cos(y), 2) + 12) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(x), 2) + 9);
+                 ? Math.Pow(1 + (y + 2) / Math.Pow(x, 2), x)
+                 : (2 * Math.Pow(x, 2) - Math.Pow(Math.Cos(y), 2) + 12) / (Math.Pow(y, 2) - Math.Pow(Math.Sin(x), 2) + 9);
 
             return Math.Round(z, 3);
         }
--- Tyuiu.ShahabA.Sprint2.Task4.V28.Test/DataServiceTest.cs
+++ Tyuiu.ShahabA.Sprint2.Task4.V28.Test/DataServiceTest.cs
@@ -10,11 +10,13 @@ namespace Tyuiu.ShahabA.Sprint2.Task4.V28.Test
         public void ValidCondition1()
         {
             DataService ds = new DataService();
-            double x = 10;
-            double y = -5;
-            double res = ds.Calculate(x, y);
-            double wait = 3.5;
-            Assert.AreEqual(wait, res);
+            double x = 2;
+            double y = 10;
+            // x < y*2 - 10 → 2 < 20-10 → 2 < 10 → True
+            // z = (1 + (10+2)/4)^2 = (1 + 12/4)^2 = (1 + 3)^2 = 16
+            double wait = 16.0;
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(wait, result);
         }
 
         [TestMethod]
@@ -22,10 +24,12 @@ namespace Tyuiu.ShahabA.Sprint2.Task4.V28.Test
         {
             DataService ds = new DataService();
             double x = 10;
-            double y = -15;
-            double res = ds.Calculate(x, y);
-            double wait = 0.167;
-            Assert.AreEqual(wait, res, 0.001);
+            double y = 5;
+            // x < y*2 - 10 → 10 < 10-10 → 10 < 0 → False
+            // z = (2*100 - cos(5)² + 12) / (25 - sin(10)² + 9)
+            double wait = 1.923;
+            double result = ds.Calculate(x, y);
+            Assert.AreEqual(wait, result, 0.001);
         }
     }
 }
